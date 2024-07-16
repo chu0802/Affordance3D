9148fc5baef848ab79e1330b3a397d07420a3b8b
chu0802 <r09922104@ntu.edu.tw>
2024-07-16 11:35:43 +0800

    <update>: update geoaware_api for deriving semantic correspondence

--- src/geoaware_api.py
+++ src/geoaware_api.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from odise.modeling.wrapper import OpenPanopticInference
 from PIL import Image
 from torchvision.transforms import Resize
-
+import torch.distributed.pipeline.sync as sync
 from .geoaware.model_utils.extractor_dino import ViTExtractor
 from .geoaware.model_utils.extractor_sd import (
     build_demo_classes_and_metadata,
@@ -12,47 +12,77 @@ from .geoaware.model_utils.extractor_sd import (
 )
 from .geoaware.model_utils.projection_network import AggregationNetwork
 from .geoaware.utils.utils_correspondence import resize
-
+import os
 # def resize(img, size=224):
 #     return Resize((size, size))(img)
 
 
 def get_sd_features(img, sd_model, num_patches=60):
     img_sd_input = Resize(num_patches * 16)(img)
-
-    demo_classes, demo_metadata = build_demo_classes_and_metadata(
-        vocab="", label_list=["COCO"]
-    )
-
-    inference_model = OpenPanopticInference(
-        model=sd_model,
-        labels=demo_classes,
-        metadata=demo_metadata,
-        semantic_on=False,
-        instance_on=False,
-        panoptic_on=True,
-    )
-    # TODO: set require_grad of inference_model to false
-    inference_model.eval()
-
-    for param in inference_model.parameters():
-        param.requires_grad = False
-
     height, width = img_sd_input.shape[-2:]
-    return inference_model.get_features(
+
+    features_sd = sd_model.get_features(
         [{"image": img_sd_input, "height": height, "width": width}], pca=True
     )
-
-
-def get_correspondence_features(
-    img, sd_model, aggre_net, extractor_vit, num_patches=60
-):
-    features_sd = get_sd_features(img, sd_model, num_patches=num_patches)
+    
+    del features_sd["s2"]
+    
     return features_sd
 
+def get_dino_features(img, extractor_vit, num_patches=60):
+    img_dino_input = Resize(num_patches * 14)(img)
+    
+    img_batch = (extractor_vit.preprocess_torch_image(img_dino_input))
+
+    features_dino = extractor_vit.extract_descriptors(
+        img_batch, layer=11, facet="token"
+    )
+    features_dino = features_dino.permute(0, 1, 3, 2).reshape(
+        1, -1, num_patches, num_patches
+    )
+    
+    return features_dino
+
+def _get_correspondence_features(
+    img, sd_model, aggre_net, extractor_vit, num_patches=60
+): 
+    features_sd = get_sd_features(img, sd_model, num_patches=num_patches)
+    features_dino = get_dino_features(img, extractor_vit, num_patches)
+
+    desc_gathered = torch.cat(
+        [
+            features_sd["s3"],
+            F.interpolate(
+                features_sd["s4"],
+                size=(num_patches, num_patches),
+                mode="bilinear",
+                align_corners=False,
+            ),
+            F.interpolate(
+                features_sd["s5"],
+                size=(num_patches, num_patches),
+                mode="bilinear",
+                align_corners=False,
+            ),
+            features_dino,
+        ],
+        dim=1,
+    )
+
+    desc = aggre_net(desc_gathered)  # 1, 768, 60, 60
+    
+    norms_desc = torch.linalg.norm(desc, dim=1, keepdim=True)
+    desc = desc / (norms_desc + 1e-8)
+    return desc
+
+
+def get_correspondence_features(img, sd_model, aggre_net, extractor_vit, num_patches=60):
+    with torch.no_grad():
+        return _get_correspondence_features(img, sd_model, aggre_net, extractor_vit, num_patches)
+    
 
 def get_processed_features(
-    img, sd_model, sd_aug, aggre_net, extractor_vit, num_patches=60, mode="pil"
+    img, sd_model, sd_aug, aggre_net, extractor_vit, num_patches=60
 ):
     # extract stable diffusion features
     img_sd_input = resize(img, target_res=num_patches * 16, resize=True, to_pil=True)
@@ -99,25 +129,31 @@ def get_processed_features(
     return desc
 
 
-def init_geoaware_models(aggre_net_path="results_spair/best_856.PTH", num_patches=60):
+def init_geoaware_models(aggre_net_path="results_spair/best_856.PTH", num_patches=60, geoaware_device="cuda:1"):
     sd_model, sd_aug = load_model(
-        diffusion_ver="v1-5", image_size=num_patches * 16, num_timesteps=50
+        diffusion_ver="v1-5", image_size=num_patches * 16, num_timesteps=50, device=geoaware_device
     )
-    extractor_vit = ViTExtractor("dinov2_vitb14", stride=14, device="cuda")
+    extractor_vit = ViTExtractor("dinov2_vitb14", stride=14, device=geoaware_device)
     aggre_net = AggregationNetwork(
-        feature_dims=[640, 1280, 1280, 768], projection_dim=768, device="cuda"
+        feature_dims=[640, 1280, 1280, 768], projection_dim=768, device=geoaware_device
     )
     aggre_net.load_pretrained_weights(torch.load(aggre_net_path))
 
-    return sd_model, aggre_net, extractor_vit
-
+    return sd_model, sd_aug, aggre_net, extractor_vit
 
 if __name__ == "__main__":
-
-    img = Image.open(
-        "data/log_genzi/quintyn-glenn-city-scene-kyoto/walk-bridge-1/view_0.png"
+    num_patches = 60
+    device = torch.device("cuda:0")
+    aggre_net_path = "/home/chu980802/geoaware/results_spair/best_856.PTH"
+    img = Image.open("/home/chu980802/Affordance3D/reference_image.png")
+    
+    sd_model, sd_aug = load_model(
+        diffusion_ver="v1-5", image_size=num_patches * 16, num_timesteps=50, device=device
     )
-    models = init_geoaware_models(
-        aggre_net_path="/home/chuyu/merced/geoaware/results_spair/best_856.PTH"
+    extractor_vit = ViTExtractor("dinov2_vitb14", stride=14, device=device)
+    aggre_net = AggregationNetwork(
+        feature_dims=[640, 1280, 1280, 768], projection_dim=768, device=device
     )
-    print(get_processed_features(img, *models))
+    aggre_net.load_pretrained_weights(torch.load(aggre_net_path))
+    
+    get_processed_features(img, sd_model, sd_aug, aggre_net, extractor_vit, num_patches)
